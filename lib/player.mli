2a4f56766e65de2432a7112a6f0bb1c8d5eee13d
Pedro Pontes García <pp457@cornell.edu>
2023-10-19 17:08:31 -0400

    Updated Player module interface:
    - Updated documentation with authors and parameters
    - Temporarily exposed player type to debug collision behaviour

--- lib/player.mli
+++ lib/player.mli
@@ -1,15 +1,34 @@
 (** Player module. Includes functions to initialize, update state and draw a
-    player. *)
-type t
+    player.
 
-(** Returns new player *)
+    Authors: Luis Hernández Rocha, Pedro Pontes García, Tawakalt Bisola Okunola. *)
+
+(** Represents a player. *)
+type t = {
+  pos : Vector.t;
+  vel : Vector.t;
+  src_rect : Sdlrect.t option;
+  mutable texture : Sdltexture.t option;
+  mutable rect : Sdlrect.t option;
+  mutable on_ground : bool;
+  mutable time_on_ground : int;
+}
+
+(** Returns new player. *)
 val new_player : t
 
-(** Initializes player with given texture *)
+(** Initializes player with given texture.
+    - Parameter [texture]: initial texture for the player.
+    - Parameter [player]: player. *)
 val init_player : Sdltexture.t -> t -> unit
 
-(** Updates player state from keyboard state and time difference *)
+(** Updates player state.
+    - Parameter [keyboard]: keyboard to read state from.
+    - Parameter [dt]: time elapsed since last update.
+    - Parameter [player]: player. *)
 val update_player_state : Keyboard.t -> int -> t -> unit
 
-(** Draws player to renderer *)
+(** Draws player to renderer.
+    - Parameter [renderer]: renderer to draw on.
+    - Parameter [player]: player. *)
 val draw_player : Sdlrender.t -> t -> unit
