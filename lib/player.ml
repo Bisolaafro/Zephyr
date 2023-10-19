698df673d577bd2f294574893d5bd230049b5efe
Pedro Pontes García <pp457@cornell.edu>
2023-10-19 17:11:03 -0400

    Updated Player module implementation:
    - Extracted 'magic numbers' to constants at the top
    - Moved rectangle logic from draw_player to update_player
    - Added jump cooldown to prevent the player from jumping immediately after landing
    - Updated gravity logic

--- lib/player.ml
+++ lib/player.ml
@@ -1,73 +1,68 @@
-(** Player module. Contains a player type as well as functions to modify or read
-    fields from a player *)
-
 open Keyboard
-
-(* Redefine mod operator because it has strange behavior in OCaml *)
-let ( mod ) x y =
-  let result = x mod y in
-  if result >= 0 then result else result + y
-
-(* Simple vector type *)
-type vector = {
-  mutable x : float;
-  mutable y : float;
-}
+open Vector
 
 type t = {
-  pos : vector;
-  vel : vector;
-  src_rect : Sdlrect.t;
+  pos : Vector.t;
+  vel : Vector.t;
+  src_rect : Sdlrect.t option;
   mutable texture : Sdltexture.t option;
-  mutable rect : Sdlrect.t;
+  mutable rect : Sdlrect.t option;
   mutable on_ground : bool;
+  mutable time_on_ground : int;
 }
 
-let ground_level = 256.
+let ground_level = 245.
+let accel = 0.005
+let dx = 4.
+let dvx = 0.2
+let dvy = 2.
+let jump_cooldown = 7
+let width, height = (1920, 1080)
 
 let new_player =
   {
     pos = { x = 0.; y = ground_level };
     vel = { x = 0.; y = 0. };
-    src_rect = Sdlrect.make4 ~x:0 ~y:0 ~w:256 ~h:256;
+    src_rect = Some (Sdlrect.make4 ~x:0 ~y:0 ~w:264 ~h:174);
     texture = None;
-    rect = Sdlrect.make4 ~x:0 ~y:0 ~w:256 ~h:256;
+    rect = Some (Sdlrect.make4 ~x:0 ~y:0 ~w:264 ~h:174);
     on_ground = true;
+    time_on_ground = 0;
   }
 
 let init_player texture t = t.texture <- Some texture
 
 let update_player_state k dt t =
   let float_dt = float_of_int dt in
-  t.on_ground <- t.pos.y <= ground_level;
-  let accel = 0.005 in
-  if query_key W k then t.pos.y <- t.pos.y +. 0.;
-  if query_key S k then t.pos.y <- t.pos.y -. 0.;
+  let accel = accel in
+  t.pos.y <- t.pos.y +. (t.vel.y *. float_dt) -. (accel *. (float_dt ** 2.));
+  t.vel.y <- t.vel.y -. (accel *. float_dt);
+  t.pos.x <- t.pos.x +. (t.vel.x *. float_dt);
+  if t.pos.y <= ground_level then (
+    t.on_ground <- true;
+    t.pos.y <- ground_level);
   if query_key A k then (
-    t.pos.x <- t.pos.x -. 4.;
-    t.vel.x <- -0.1);
+    t.pos.x <- t.pos.x -. dx;
+    t.vel.x <- -.dvx);
   if query_key D k then (
-    t.pos.x <- t.pos.x +. 4.;
-    t.vel.x <- 0.1);
-  if query_key Space k && t.on_ground then (
+    t.pos.x <- t.pos.x +. dx;
+    t.vel.x <- dvx);
+  if query_key Space k && t.on_ground && t.time_on_ground >= jump_cooldown then (
     t.vel.x <- 0.;
-    t.vel.y <- 1.;
-    t.on_ground <- false);
+    t.vel.y <- dvy;
+    t.on_ground <- false;
+    t.time_on_ground <- 0);
   if t.on_ground then (
-    t.vel.y <- 0.;
     t.vel.x <- 0.;
-    t.pos.y <- ground_level)
-  else (
-    t.pos.y <- t.pos.y +. (t.vel.y *. float_dt) -. (accel *. (float_dt ** 2.));
-    t.vel.y <- t.vel.y -. (accel *. float_dt));
-  t.pos.x <- t.pos.x +. (t.vel.x *. float_dt)
+    t.time_on_ground <- t.time_on_ground + 1);
+  t.rect <-
+    Some
+      {
+        (Option.get t.rect) with
+        x = int_of_float t.pos.x;
+        y = int_of_float (float_of_int height -. t.pos.y);
+      }
 
 let draw_player r t =
-  t.rect <-
-    {
-      t.rect with
-      x = int_of_float t.pos.x mod 1920;
-      y = int_of_float (float_of_int 1080 -. t.pos.y);
-    };
-  Sdlrender.copy r ~texture:(Option.get t.texture) ~src_rect:t.src_rect
-    ~dst_rect:t.rect ()
+  Sdlrender.copy r ~texture:(Option.get t.texture)
+    ~src_rect:(Option.get t.src_rect) ~dst_rect:(Option.get t.rect) ()
