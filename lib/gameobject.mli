332980f0979f6c9e5b928113225be0e4b489cf02
Pedro Pontes García <pp457@cornell.edu>
2023-10-20 01:21:31 -0400

    Abstracted old Obstacle module as well as Player functionality into new Gameobject module
    - Gameobject handles updates such as gravity or ground collision
    - Gameobject handles drawing
    - In essence, Gameobject replaces the need for Obstacle

---
+++ lib/gameobject.mli
@@ -0,0 +1,37 @@
+(** Game object module. Includes a type to represent a game object and functions
+    to initialize them, update their state, and draw them. *)
+
+(** Represents a game object. *)
+type t = {
+  pos : Vector.t;
+  vel : Vector.t;
+  mutable width : float;
+  mutable height : float;
+  mutable texture : Sdltexture.t option;
+  mutable src_rect : Sdlrect.t option;
+  mutable rect : Sdlrect.t option;
+  mutable affected_by_gravity : bool;
+  mutable on_ground : bool;
+}
+
+(** Returns new game object. *)
+val new_object : unit -> t
+
+(** Initializes obstacle with given texture.
+    - Parameter [texture]: initial texture for the object.
+    - Parameter [bottom_left]: initial bottom-left corner.
+    - Parameter [top_right]: initial top-right corner.
+    - Parameter [gravity]: whether the game object is affected by gravity.
+    - Parameter [obj] game object. *)
+val init_object :
+  Sdltexture.t -> float * float -> float * float -> bool -> t -> unit
+
+(** Updates object state.
+    - Parameter [dt]: time elapsed since last update.
+    - Parameter [obj]: game object. *)
+val update_object_state : int -> t -> unit
+
+(** Draws game object to renderer.
+    - Parameter [renderer]: renderer to draw on.
+    - Parameter [obj]: game object. *)
+val draw_object : Sdlrender.t -> t -> unit
