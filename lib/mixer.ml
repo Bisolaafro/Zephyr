1088b273bbc9957d28d8fce42bf98e8776e8ddc0
Luis Hernandez <lh555@cornell.edu>
2023-12-05 12:25:35 -0500

    Implement Mixer Module
     - Implement Mixer module, which wraps around the
       SDL_Mixer library and provides functionaly to play sounds.
     - My mixer module abstracts away some of the complexity
       of the original Mixer module to allow for playing files
       more easily.

---
+++ lib/mixer.ml
@@ -0,0 +1,191 @@
+type audio_format =
+  | AUDIO_S8
+  | AUDIO_U8
+  | AUDIO_S16LSB
+  | AUDIO_S16MSB
+  | AUDIO_S16SYS
+  | AUDIO_U16
+  | AUDIO_S32LSB
+  | AUDIO_S32MSB
+  | AUDIO_S32SYS
+  | AUDIO_S32
+  | AUDIO_F32LSB
+  | AUDIO_F32MSB
+  | AUDIO_F32SYS
+  | AUDIO_F32
+  | MIX_DEFAULT_FORMAT
+
+type init_flag =
+  | FLAC
+  | MOD
+  | MP3
+  | OGG
+  | MID
+  | OPUS
+  | WAVPACK
+
+let init_flag_convert =
+  let open Tsdl_mixer.Mixer.Init in
+  function
+  | FLAC -> flac
+  | MOD -> modplug
+  | MP3 -> mp3
+  | OGG -> ogg
+  | _ -> failwith "NOT SUPPORTED"
+
+let my_format_to_tsdl =
+  let open Tsdl.Sdl.Audio in
+  function
+  | AUDIO_S8 -> s8
+  | AUDIO_U8 -> u8
+  | AUDIO_S16LSB -> s16_lsb
+  | AUDIO_S16MSB -> s16_msb
+  | AUDIO_S16SYS -> s16_sys
+  | AUDIO_U16 -> u16
+  | AUDIO_S32LSB -> s32_lsb
+  | AUDIO_S32MSB -> s32_msb
+  | AUDIO_S32SYS -> s32_sys
+  | AUDIO_S32 -> s32
+  | AUDIO_F32LSB -> f32_lsb
+  | AUDIO_F32MSB -> f32_msb
+  | AUDIO_F32SYS -> f32_sys
+  | AUDIO_F32 -> f32
+  | MIX_DEFAULT_FORMAT -> s16_lsb
+
+(* Init function *)
+let init lst =
+  List.fold_left
+    (fun _ x -> ignore (Tsdl_mixer.Mixer.init (init_flag_convert x)))
+    () lst
+
+(* Exception*)
+exception ReadError of string
+
+(**** Module TYPES ****)
+module type MusicModType = sig
+  type t
+
+  val play_music : t -> int -> unit
+  val free_music : t -> unit
+  val load_music : string -> t
+  val music_vol : int -> int
+end
+
+module type ChunkModType = sig
+  type t
+
+  val load_wav : string -> t
+  val chunk_vol : t -> int -> int
+end
+
+module type ChannelTrackerType = sig
+  type t
+
+  val create_tracker : int -> t
+  val get_reserved : t -> int
+  val reserve_channels : t -> Gameobject.GameObject.t -> int -> unit
+  val get_obj_alloc : t -> Gameobject.GameObject.t -> int list option
+  val free_obj : t -> Gameobject.GameObject.t -> unit
+end
+
+(** END OF TYPES **)
+
+(* Implementation of Music*)
+
+module Music : MusicModType = struct
+  type t = Tsdl_mixer.Mixer.music
+
+  let play_music t n =
+    match Tsdl_mixer.Mixer.play_music t n with
+    | Ok _ -> ()
+    | Error (`Msg str) -> raise (ReadError str)
+
+  let free_music = Tsdl_mixer.Mixer.free_music
+
+  let load_music str =
+    match Tsdl_mixer.Mixer.load_mus str with
+    | Ok s -> s
+    | Error (`Msg str) -> raise (ReadError str)
+
+  let music_vol = Tsdl_mixer.Mixer.volume_music
+end
+
+(* End of Music implementation*)
+
+module Chunk : ChunkModType with type t = Tsdl_mixer.Mixer.chunk = struct
+  type t = Tsdl_mixer.Mixer.chunk
+
+  let load_wav str =
+    match Tsdl_mixer.Mixer.load_wav str with
+    | Ok s -> s
+    | Error (`Msg str) -> raise (ReadError str)
+
+  let chunk_vol = Tsdl_mixer.Mixer.volume_chunk
+end
+
+module ChannelTracker : ChannelTrackerType = struct
+  type t = {
+    size : int;
+    mutable reserved : int;
+    arr : Gameobject.GameObject.t ref option Array.t;
+    hash : (Gameobject.GameObject.t, int list) Hashtbl.t;
+  }
+
+  let create_tracker n =
+    {
+      size = n;
+      reserved = 0;
+      arr = Array.make n None;
+      hash = Hashtbl.create 35;
+    }
+
+  let get_reserved { reserved } = reserved
+
+  let update_lst hash obj num =
+    match Hashtbl.find_opt hash obj with
+    | None -> Hashtbl.replace hash obj [ num ]
+    | Some t -> Hashtbl.replace hash obj (num :: t)
+
+  let reserve_channels t obj num =
+    let num_ref = ref num in
+    let ind = ref 0 in
+    let _ = t.reserved <- t.reserved + num in
+    while !num_ref > 0 do
+      let _ =
+        match t.arr.(!ind) with
+        | None ->
+            t.arr.(!ind) <- Some (ref obj);
+            update_lst t.hash obj num;
+            num_ref := !num_ref - 1
+        | Some _ -> ()
+      in
+      ind := !ind + 1
+    done
+
+  let get_obj_alloc { hash } obj = Hashtbl.find_opt hash obj
+
+  (* Think about structural or physical comparison, currently using physical *)
+  let compare_obj_in_arr obj1 obj2 =
+    match obj1 with
+    | Some obj1_unwr -> !obj1_unwr == obj2
+    | _ -> false
+
+  let free_obj t obj =
+    Array.iteri
+      (fun i obj_wr ->
+        if compare_obj_in_arr obj_wr obj then t.arr.(i) <- None;
+        t.reserved <- t.reserved - 1)
+      t.arr
+end
+
+let play_channel c chunk l =
+  match Tsdl_mixer.Mixer.play_channel c chunk l with
+  | Ok _ -> ()
+  | Error _ -> failwith "SOMETHING HAPPENED WHEN PLAYING CHANNEL"
+
+let allocate_channels = Tsdl_mixer.Mixer.allocate_channels
+let volume = Tsdl_mixer.Mixer.volume
+
+let open_audio freq format chan chunk =
+  ignore
+    (Tsdl_mixer.Mixer.open_audio freq (my_format_to_tsdl format) chan chunk)
---
+++ lib/mixer.mli
@@ -0,0 +1,162 @@
+(** This module wraps around the library [Tsdl_mixer] library, which
+    interoperates with the other Ocaml bindings for [SDL] titled [Tsdl].
+    Miraculously, [Tsdl_mixer] is able to function in tandem with [OcamlSDL2],
+    and this module facilitates playing music and sound effects, amongst other
+    things. *)
+
+(** Audio formats supported by the [SDL_mixer] library. More information about
+    their specifics can be found in the SDL documentation. *)
+type audio_format =
+  | AUDIO_S8
+  | AUDIO_U8
+  | AUDIO_S16LSB
+  | AUDIO_S16MSB
+  | AUDIO_S16SYS
+  | AUDIO_U16
+  | AUDIO_S32LSB
+  | AUDIO_S32MSB
+  | AUDIO_S32SYS
+  | AUDIO_S32
+  | AUDIO_F32LSB
+  | AUDIO_F32MSB
+  | AUDIO_F32SYS
+  | AUDIO_F32
+  | MIX_DEFAULT_FORMAT
+
+(** Flags passed to [init] that tell SDL_mixer what libraries to load.*)
+type init_flag =
+  | FLAC
+  | MOD
+  | MP3
+  | OGG
+  | MID
+  | OPUS
+  | WAVPACK
+
+(** Exception that is raised when files could not be read. *)
+exception ReadError of string
+
+(** This function loads dynamic libraries that SDL_mixer needs, and prepares
+    them for use. Unlike other SDL libraries, this function call is optional.
+    For more information, see the API documentation of the function. Ideally,
+    should not even use this function.*)
+val init : init_flag list -> unit
+
+(** Module that facilitates music playing. That is, a single music channel is
+    usually played for an expended period of time, usually multiple times. *)
+module type MusicModType = sig
+  (* A loaded music file that can be played. *)
+  type t
+
+  (** [play_music m n] plays the music object [m], and plays it in a loop [n]
+      times. That is, [n=0] means play music once and then stop. *)
+  val play_music : t -> int -> unit
+
+  (** [free_music m] frees the music object [m]. If the music object is
+      currently playing, it will be stopped. Once a music object is not being
+      used, you should free it. *)
+  val free_music : t -> unit
+
+  (** [load_music m] creates a music object by loading it from the directory
+      provided in [m]. This must happen after [open_audio] has been called.
+      Raises: [ReadError] if the file could not be read. *)
+  val load_music : string -> t
+
+  (** [music_vol n] sets the volume of the music channel and returns the
+      previous value. The maximum volume is [128], anything higher than that
+      will simply default to [128]. Passing a negative number will simply return
+      the current volume without changing it. *)
+  val music_vol : int -> int
+end
+
+(** Implementation of MusicModType *)
+module Music : MusicModType
+
+(** A chunk is the other type of audio data structure, in addition to
+    [MusicModType], which is meant to be a file completely decoded into memory
+    up front. In contrast to [MusicModType.t], an indefinite number can play at
+    a time.*)
+module type ChunkModType = sig
+  (** A loaded chunk file that can be played. These usually are [.wav] files. *)
+  type t
+
+  (** [load_wav s] loads a supported audio file into a chunk, where [s] is the
+      location of the file. Raises: [ReadError] if the file could not be read. *)
+  val load_wav : string -> t
+
+  (** [chunk_vol ch vol] sets the volume of chunk [ch] to [vol] and returns the
+      previous value. [vol] is usually between [0] and [128]. Passing a negative
+      number will simply return the current volume, while something greater will
+      default to [128]. *)
+  val chunk_vol : t -> int -> int
+end
+
+(*** Implementation of ChunkModType*)
+module Chunk : ChunkModType
+
+(** [open_audio freq format chan chunk] opens the default audio device for
+    playback. In other words, this function is the actual thing that makes
+    'sound.'
+
+    - Parameter [freq]: the frequency to playback audio at (in Hz) (44100 is
+      good)
+
+    - Parameter [format]: one of [SDL]'s audio format values. Should usually use
+      [MIX_DEFAULT_FORMAT]
+
+    - Parameter [chan]: number of channels (1 is mono, 2 is stereo), usually
+      choose [1] or [2].
+
+    - Parameter [chunk]: audio buffer size in sample FRAMES. (2048 is good) *)
+val open_audio : int -> audio_format -> int -> int -> unit
+
+(** [play_channel c chunk l] plays the chunk [chunk] on the channel number [c]
+    [l] times. Passing [c] as [-1] will play the chunk on the first available
+    channel. *)
+val play_channel : int -> Chunk.t -> int -> unit
+
+(** [allocate num] allocates [num] channels for playing audio track and returns
+    the number itself. Note that this is distinct from the mono,stereo, etc,
+    meaning of channel. This refers to the number of tracks that can
+    simultaneously be played. *)
+val allocate_channels : int -> int
+
+(** [volume ch vol] sets the volume of the channel [ch] to [vol]. Note that
+    [vol] is usually between [0] and [128]. Passing [-1] for vol will simply
+    return the current volume without making any changes.*)
+val volume : int -> int -> int
+
+(** A ChannelTrackerType module provides functionality that allows the user to
+    easily add sound effects to gameobjects by abstracting away the *)
+module type ChannelTrackerType = sig
+  (** Keeps track of the channels currently allocated in the game.*)
+  type t
+
+  (** [create_tracker num] creates a tracker that keeps track of [num] channels.
+      This functions independently of the [allocate_channels] function.
+      Requires: [num] be the number of allocated channels using
+      [allocate_channels]. *)
+  val create_tracker : int -> t
+
+  (** [get_reserved t] is the number of tracks reserved for all game objects in
+      the tracker [t]. *)
+  val get_reserved : t -> int
+
+  (** [reserve_channels t obj num] allocates [num] channels for the [obj]
+      GameObject on the tracker [t]. Requires: adding [num] to the already
+      reserved channels must not exceed the total number of tracks already
+      allocated. *)
+  val reserve_channels : t -> Gameobject.GameObject.t -> int -> unit
+
+  (** [get_obj_alloc t obj] returns a list with channels allocated to the game
+      object [obj]. Note that these numbers can be safely used with
+      [play_channel]. In fact, this is the intended use. *)
+  val get_obj_alloc : t -> Gameobject.GameObject.t -> int list option
+
+  (** [free_obj tr obj] removes the channel reservation of [obj] and makes it
+      available to other objects.*)
+  val free_obj : t -> Gameobject.GameObject.t -> unit
+end
+
+(** Implementation of ChannelTrackerType *)
+module ChannelTracker : ChannelTrackerType
