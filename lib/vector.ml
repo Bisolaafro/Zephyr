83b836c4750db5b7e7762472c783c4658080160b
Luis Hernandez <lh555@cornell.edu>
2023-12-05 12:28:08 -0500

    Add more functionality to Vector module.
     - Added several functions that should facilitate
       doing vector operations.
     - Some of them include finding the length of a
       vector, and scaling it by a float.

--- lib/vector.ml
+++ lib/vector.ml
@@ -4,3 +4,15 @@ type t = {
 }
 
 let zero _ = { x = 0.; y = 0. }
+let add v1 v2 = { x = v1.x +. v2.x; y = v1.y +. v2.y }
+
+let scale n v =
+  v.x <- n *. v.x;
+  v.y <- n *. v.y
+
+let length v = sqrt ((v.x *. v.x) +. (v.y *. v.y))
+
+let normalize v =
+  let len = length v in
+  v.x <- v.x /. len;
+  v.y <- v.y /. len
--- lib/vector.mli
+++ lib/vector.mli
@@ -1,4 +1,5 @@
-(** Simple vector module. *)
+(** Simple vector module that provides functionalty for mutable, two-dimensional
+    vector operations. *)
 
 (** Represents a vector. *)
 type t = {
@@ -8,3 +9,20 @@ type t = {
 
 (** Returns the zero vector. *)
 val zero : unit -> t
+
+(** [add v1 v2] is the point-wise addition of vectors [v1] and [v2]. This
+    function does not modify either of the original vectors. *)
+val add : t -> t -> t
+
+(** [scale n v] scales each component of [v] by [n]. *)
+val scale : float -> t -> unit
+
+(** [normalize v] modifies [v] so that it points in the same direction but with
+    unit length. That is, if [v] has components [v1] and [v2], then
+    [normalize v] is the vector with components [[v1/sqrt(v1^2 + v2^2)]] and
+    [[v2/sqrt(v1^2 + v2^2)]]. Requires: [v] cannot be the zero vector. *)
+val normalize : t -> unit
+
+(** [normalize v] is the square root of the sum of the squared components of
+    [v]. *)
+val length : t -> float
